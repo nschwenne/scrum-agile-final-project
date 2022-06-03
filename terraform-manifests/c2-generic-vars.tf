9d331e52b0fba157c9457001585e02cad6a76b54
David Schepis <davidschepis@gmail.com>
2022-06-03 12:18:46 -0700

    updated mysql files

--- terraform-manifests/c2-generic-vars.tf
+++ terraform-manifests/c2-generic-vars.tf
@@ -1,15 +1,13 @@
 # Generic Variables for Terraform Provisioning
 
-
 variable "resource_group_name" {
   description = "Resource Group Name"
-  type = string
-  default = group1_wp_deployment_rg 
+  type        = string
+  default     = group1_wp_deployment_rg
 }
 
 variable "resource_group_location" {
   description = "Location of Resource Group"
-  type = string
-  default = "centralus"
+  type        = string
+  default     = "centralus"
 }
-
