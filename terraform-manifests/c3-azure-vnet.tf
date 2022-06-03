405adafe494866361442c998930883c579df121f
Andreas-Wall <60330641+Andreas-Wall@users.noreply.github.com>
2022-06-03 14:45:24 -0500

    Update c3-azure-vnet.tf

--- terraform-manifests/c3-azure-vnet.tf
+++ terraform-manifests/c3-azure-vnet.tf
@@ -29,6 +29,26 @@ resource "azurerm_network_interface" "windows_network_interface" {
   }
 }
 
+resource "azurerm_public_ip" "public_ip" {
+  name                = "publicip"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku = "Standard"
+}
+
+
+resource "azurerm_lb" "project_load_balancer" {
+  name                = "projectlb"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  sku = "Standard"
+  frontend_ip_configuration {
+    name                 = "load-balancer-publicip"
+    public_ip_address_id = azurerm_public_ip.public_ip
+  }
+}
+
 resource "azurerm_windows_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.windows_resource_group.name
