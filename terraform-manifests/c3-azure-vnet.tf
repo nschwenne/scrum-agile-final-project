8b1b449c5f629716bd62bcf49723f018b7a34c45
mphelps <michaeljphelps1978@gmail.com>
2022-06-05 14:02:27 +0000

    Added NSG and firewall rules to vnet

--- terraform-manifests/c3-azure-vnet.tf
+++ terraform-manifests/c3-azure-vnet.tf
@@ -29,3 +29,40 @@ resource "azurerm_network_interface" "client_nic" {
     
   }
 }
+
+resource "azurerm_network_security_group" "wp_subnet_nsg" {
+  name                = "wp_subnet_nsg"
+  location            = azurerm_resource_group.arm_rg.location
+  resource_group_name = azurerm_resource_group.arm_rg.name
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "wordpress_nsg_associate" {
+  depends_on = [ azurerm_network_security_rule.wordpress_nsg_ingress_rules] # Every NSG Rule Association will disassociate NSG from Subnet and Associate it, so we associate it only after NSG is completely created - Azure Provider Bug https://github.com/terraform-providers/terraform-provider-azurerm/issues/354    
+  subnet_id                 = azurerm_subnet.client_subnet.id
+  network_security_group_id = azurerm_network_security_group.wp_subnet_nsg.id
+}
+
+locals {
+  web_inbound_ports_map = {
+    "100" : "80", 
+    "110" : "443",
+    "120" : "22",
+    "130" : 3306 # MySQL Connection
+  } 
+}
+
+resource "azurerm_network_security_rule" "wordpress_nsg_ingress_rules" {
+  for_each = local.web_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value 
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.arm_rg.name
+  network_security_group_name = azurerm_network_security_group.wp_subnet_nsg.name
+}
