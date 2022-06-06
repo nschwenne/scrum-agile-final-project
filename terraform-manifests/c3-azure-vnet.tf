57b007091aeccf59494c307ea34fd1464aefe479
mphelps <michaeljphelps1978@gmail.com>
2022-06-06 14:42:40 +0000

    fixed resource naming issues, added nsg and firewall

--- terraform-manifests/c3-azure-vnet.tf
+++ terraform-manifests/c3-azure-vnet.tf
@@ -19,8 +19,8 @@ resource "azurerm_subnet" "windows_azurerm_subnet" {
 
 resource "azurerm_network_interface" "windows_network_interface" {
   name                = "windows-nic"
-  location            = azurerm_resource_group.windows_virtual_network.location
-  resource_group_name = azurerm_resource_group.windows_virtual_network.name
+  location            = azurerm_resource_group.windows_resource_group.location
+  resource_group_name = azurerm_resource_group.windows_resource_group.name
 
   ip_configuration {
     name                          = "internal"
@@ -50,6 +50,9 @@ resource "azurerm_lb" "project_load_balancer" {
 }
 
 resource "azurerm_windows_virtual_machine" "example" {
+  depends_on = [
+    azurerm_network_interface.windows_network_interface
+  ]
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.windows_resource_group.name
   location            = azurerm_resource_group.windows_resource_group.location
@@ -57,7 +60,7 @@ resource "azurerm_windows_virtual_machine" "example" {
   admin_username      = "adminuser"
   admin_password      = "j0hnth3f1sh3rman"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.windows_network_interface.id
   ]
 
   os_disk {
@@ -71,4 +74,41 @@ resource "azurerm_windows_virtual_machine" "example" {
     sku       = "2016-Datacenter"
     version   = "latest"
   }
+}
+
+resource "azurerm_network_security_group" "wp_subnet_nsg" {
+  name                = "wp_subnet_nsg"
+  location            = azurerm_resource_group.windows_resource_group.location
+  resource_group_name = azurerm_resource_group.windows_resource_group.name
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "wordpress_nsg_associate" {
+  depends_on = [ azurerm_network_security_rule.wordpress_nsg_ingress_rules]
+  subnet_id                 = azurerm_subnet.windows_azurerm_subnet.id
+  network_security_group_id = azurerm_network_security_group.wp_subnet_nsg.id
+}
+
+locals {
+  web_inbound_ports_map = {
+    "100" : "80", 
+    "110" : "443",
+    "120" : "22",
+    "130" : 3306 # MySQL Connection
+  } 
+}
+
+resource "azurerm_network_security_rule" "wordpress_nsg_ingress_rules" {
+  for_each = local.web_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value 
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.windows_resource_group.name
+  network_security_group_name = azurerm_network_security_group.wp_subnet_nsg.name
 }
