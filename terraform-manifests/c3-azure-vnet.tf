26437eb13451e61bfec3562f3e730681c7da5fd4
Nicholas  Schwenne <83676675+nschwenne@users.noreply.github.com>
2022-06-06 07:17:37 -0800

    Fixed Load balancer and Public IP
    Updated Resource_group_name and Location for the Load Balancer and the Public IP to be  azurerm_resource_group.windows_resource_group.name and azurerm_resource_group.windows_resource_group.location

--- terraform-manifests/c3-azure-vnet.tf
+++ terraform-manifests/c3-azure-vnet.tf
@@ -31,8 +31,8 @@ resource "azurerm_network_interface" "windows_network_interface" {
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "publicip"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.windows_resource_group.location
+  resource_group_name = azurerm_resource_group.windows_resource_group.name
   allocation_method   = "Static"
   sku = "Standard"
 }
@@ -40,8 +40,8 @@ resource "azurerm_public_ip" "public_ip" {
 
 resource "azurerm_lb" "project_load_balancer" {
   name                = "projectlb"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.windows_resource_group.location
+  resource_group_name = azurerm_resource_group.windows_resource_group.name
   sku = "Standard"
   frontend_ip_configuration {
     name                 = "load-balancer-publicip"
