73bac952d918aa76f3e1f98a3d607d3f4c842dae
David Schepis <davidschepis@gmail.com>
2022-06-06 09:28:36 -0700

    small bugs in c3

--- terraform-manifests/c3-azure-vnet.tf
+++ terraform-manifests/c3-azure-vnet.tf
@@ -31,8 +31,8 @@ resource "azurerm_network_interface" "windows_network_interface" {
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "publicip"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
+  location            = "centralus"
+  resource_group_name =  "windows-rg"
   allocation_method   = "Static"
   sku = "Standard"
 }
@@ -40,12 +40,12 @@ resource "azurerm_public_ip" "public_ip" {
 
 resource "azurerm_lb" "project_load_balancer" {
   name                = "projectlb"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
+  location            = "centralus"
+  resource_group_name =  "windows-rg"
   sku = "Standard"
   frontend_ip_configuration {
     name                 = "load-balancer-publicip"
-    public_ip_address_id = azurerm_public_ip.public_ip
+    public_ip_address_id = azurerm_public_ip.public_ip.id
   }
 }
 
